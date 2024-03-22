7b3d972ccda5bc949328ba434f2df178bfef04ce
Luana Thomas <luana.thomas@hotmail.com>
2024-03-22 17:01:35 -0300

    Final commit

--- MVPmoviewDB/MVPmoviewDB/View/MovieDetailViewController.swift
+++ MVPmoviewDB/MVPmoviewDB/View/MovieDetailViewController.swift
@@ -48,14 +48,14 @@ extension MovieDetailViewController {
         
         let titleLabel: UILabel = {
             let label = UILabel()
-            label.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+            label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
             label.numberOfLines = 1
             return label
         }()
         
         let overviewLabel: UILabel = {
             let label = UILabel()
-            label.font = UIFont.systemFont(ofSize: 13)
+            label.font = UIFont.systemFont(ofSize: 14)
             label.numberOfLines = 0
             label.textColor = .gray
             return label
@@ -63,7 +63,7 @@ extension MovieDetailViewController {
         
         let tagsLabel: UILabel = {
             let label = UILabel()
-            label.font = UIFont.systemFont(ofSize: 13)
+            label.font = UIFont.systemFont(ofSize: 17)
             label.numberOfLines = 3
             label.textColor = .gray
             return label
@@ -71,7 +71,7 @@ extension MovieDetailViewController {
         
         let voteAverageLabel: UILabel = {
             let label = UILabel()
-            label.font = UIFont.systemFont(ofSize: 12, weight: .light)
+            label.font = UIFont.systemFont(ofSize: 15, weight: .light)
             label.numberOfLines = 1
             label.textColor = .gray
             return label
@@ -85,20 +85,24 @@ extension MovieDetailViewController {
             return label
         }()
         
+        var voidLabel = UILabel()
+        voidLabel.text = " "
+        
         image.image = UIImage(data: movieDetailViewControllerModel.imageCover ?? Data())
         titleLabel.text = movieDetailViewControllerModel.title
         overviewLabel.text = movieDetailViewControllerModel.overview
         tagsLabel.text = movieDetailViewControllerModel.tags
         voteAverageLabel.text = "✩ " + movieDetailViewControllerModel.voteAverage
         
-        let headerVStack = UIStackView(arrangedSubviews: [titleLabel, tagsLabel, voteAverageLabel])
+        let headerVStack = UIStackView(arrangedSubviews: [voidLabel, titleLabel, tagsLabel, voteAverageLabel])
         headerVStack.axis = .vertical
         headerVStack.spacing = 16
         
+        voidLabel.setContentHuggingPriority(.required, for: .vertical)
         titleLabel.setContentHuggingPriority(.required, for: .vertical)
         tagsLabel.setContentHuggingPriority(.required, for: .vertical)
         voteAverageLabel.setContentHuggingPriority(.required, for: .vertical)
-        
+//        
         let headerHStack = UIStackView(arrangedSubviews: [image, headerVStack])
         headerHStack.axis = .horizontal
         headerHStack.spacing = 16
@@ -114,11 +118,8 @@ extension MovieDetailViewController {
         contentVStack.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             image.leadingAnchor.constraint(equalTo: headerHStack.leadingAnchor, constant: -32),
-//            image.topAnchor.constraint(equalTo: headerHStack.topAnchor, constant: 16),
             image.trailingAnchor.constraint(equalTo: headerVStack.leadingAnchor, constant: 16),
-//            image.bottomAnchor.constraint(equalTo: headerHStack.bottomAnchor, constant: 16),
-            image.widthAnchor.constraint(equalToConstant: 200),
-            image.heightAnchor.constraint(equalToConstant: 180)
+            image.widthAnchor.constraint(equalToConstant: 200)
         ])
         
         NSLayoutConstraint.activate([
@@ -153,35 +154,6 @@ extension MovieDetailViewController {
             overviewLabel.trailingAnchor.constraint(equalTo: contentVStack.trailingAnchor),
             overviewLabel.bottomAnchor.constraint(equalTo: contentVStack.bottomAnchor)
         ])
-        
-        //        headerVStack.translatesAutoresizingMaskIntoConstraints = false
-        //        headerVStack.leadingAnchor.constraint(equalTo: headerHStack.leadingAnchor, constant: 16).isActive = true
-        //        headerVStack.topAnchor.constraint(equalTo: headerHStack.topAnchor, constant: 16).isActive = true
-        //        headerVStack.trailingAnchor.constraint(equalTo: headerHStack.trailingAnchor, constant: -16).isActive = true
-        //        headerVStack.bottomAnchor.constraint(lessThanOrEqualTo: headerHStack.bottomAnchor, constant: -16).isActive = true
-        //
-        //        headerHStack.translatesAutoresizingMaskIntoConstraints = false
-        //        headerHStack.leadingAnchor.constraint(equalTo: contentVStack.leadingAnchor, constant: 16).isActive = true
-        //        headerHStack.topAnchor.constraint(equalTo: contentVStack.topAnchor, constant: 16).isActive = true
-        //        headerHStack.trailingAnchor.constraint(equalTo: contentVStack.trailingAnchor, constant: -16).isActive = true
-        //        headerHStack.bottomAnchor.constraint(lessThanOrEqualTo: contentVStack.bottomAnchor, constant: -16).isActive = true
-        //
-        //        contentVStack.translatesAutoresizingMaskIntoConstraints = false
-        //        contentVStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
-        //        contentVStack.topAnchor.constraint(equalTo: view.topAnchor, constant: 80).isActive = true
-        //        contentVStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
-        //        contentVStack.bottomAnchor.constraint(lessThanOrEqualTo: view.bottomAnchor, constant: -16).isActive = true
-        //
-        //        image.setContentHuggingPriority(.required, for: .horizontal)
-        //        contentVStack.setContentHuggingPriority(.required, for: .horizontal)
-        //
-        //        image.translatesAutoresizingMaskIntoConstraints = false
-        //        image.leadingAnchor.constraint(equalTo: headerHStack.leadingAnchor, constant: 16).isActive = true
-        //        image.topAnchor.constraint(equalTo: headerHStack.topAnchor, constant: 16).isActive = true
-        //        image.trailingAnchor.constraint(equalTo: headerVStack.leadingAnchor, constant: -16).isActive = true
-        //        image.bottomAnchor.constraint(lessThanOrEqualTo: headerHStack.bottomAnchor, constant: -16).isActive = true
-        //        image.widthAnchor.constraint(equalToConstant: 124).isActive = true // Adjust width as needed
-        
     }
 }
 
